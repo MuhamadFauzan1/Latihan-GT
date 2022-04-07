29859ed3699a834d3b08f84a608b3cbfb6d22702
MuhamadFauzan1 <101811036+MuhamadFauzan1@users.noreply.github.com>
2022-04-07 21:05:06 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,30 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fauzan</li>
+        <li>Nama Belakang : Muhamad</li>
+    </ul>
+    <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+        <li> Jalan Karang Kates VIII</li>
+    </ul>
+ 
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+        <li>Tangerang</li>
+    </ul>
+
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+        <li>Badmintoon</li>
+        <li>Rebahan</li>
+    </ul>
+</section>
+
+
+
+
+
+
